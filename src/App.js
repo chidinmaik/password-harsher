d29fd5998d6b620ff1f722e2432056cfb84a9361
chidinmaik <mhizxplendour@gmail.com>
2024-09-15 10:09:45 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ function App() {
   const [encryptedPassword, setEncryptedPassword] = useState('');
   const [decryptedPassword, setDecryptedPassword] = useState('');
 
-  // Handle password change
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
 
-  // Function to hash the password
   const hashPassword = async () => {
     try {
       const response = await fetch('http://localhost:5000/hash-password', {
@@ -32,7 +30,6 @@ function App() {
     }
   };
 
-  // Function to encrypt the password
   const encryptPassword = async () => {
     try {
       const response = await fetch('http://localhost:5000/encrypt-password', {
@@ -50,7 +47,6 @@ function App() {
     }
   };
 
-  // Function to decrypt the password
   const decryptPassword = async () => {
     try {
       const response = await fetch('http://localhost:5000/decrypt-password', {
@@ -69,36 +65,35 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="app-container">
       <NavBar />
-      <div>
-        <h1>Password Hashing & Encryption Tool</h1>
+      <div className="content-container">
+        <h1 className="title">Password Hashing & Encryption Tool</h1>
 
         <input
           type="text"
+          className="input-password"
           placeholder="Enter your password"
           value={password}
           onChange={handlePasswordChange}
         />
 
         <div className="button-group">
-          <button onTouchEnd={hashPassword} onClick={hashPassword}>Hash Password</button>
-          <button onTouchEnd={encryptPassword} onClick={encryptPassword}>Encrypt Password</button>
+          <button className="btn" onTouchEnd={hashPassword} onClick={hashPassword}>Hash Password</button>
+          <button className="btn" onTouchEnd={encryptPassword} onClick={encryptPassword}>Encrypt Password</button>
         </div>
 
         <div className="results">
           <h3>Hashed Password:</h3>
-          <textarea value={hashedPassword} readOnly rows="2" />
+          <textarea value={hashedPassword} readOnly rows="2" className="output-area" />
 
           <h3>Encrypted Password:</h3>
-          <textarea value={encryptedPassword} readOnly rows="2" />
+          <textarea value={encryptedPassword} readOnly rows="2" className="output-area" />
 
-          <button onTouchEnd={decryptPassword} onClick={decryptPassword}>Decrypt Password</button>
+          <button className="btn" onTouchEnd={decryptPassword} onClick={decryptPassword}>Decrypt Password</button>
           <h3>Decrypted Password:</h3>
-          <textarea value={decryptedPassword} readOnly rows="2" />
+          <textarea value={decryptedPassword} readOnly rows="2" className="output-area" />
         </div>
-
-       
       </div>
       <Footer />
     </div>
